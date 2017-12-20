78c741ea86c601839f6845cd1e3d869115898949
Illya Moskvin <imoskvin@artic.edu>
2017-12-20 11:24:29 -0600

    Remove unnecessary components

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        //
     }
 
     /**
@@ -35,8 +34,6 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        // $this->load(__DIR__.'/Commands');
     }
 }
