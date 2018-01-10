4a4b94b171c0722c6fdf978aed8dc782a454f0eb
Illya Moskvin <imoskvin@artic.edu>
2018-01-10 16:01:08 -0600

    Update artisan, app, and bootstrap
    This matches their structure to that used in the events and catalogues
    data services. Seems stable. We should add a copy of bootstrap/app.php
    to the foundation.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so we do not have to manually load any of
+| our application's PHP classes. It just feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
