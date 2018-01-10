e486eb42d807f8d877b5219c293c587713c8acc6
Illya Moskvin <imoskvin@artic.edu>
2018-01-10 15:58:31 -0600

    Use HttpKernel from foundation
    They are identical. This one's just shared.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,7 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Aic\Hub\Foundation\HttpKernel::class
 );
 
 $app->singleton(
